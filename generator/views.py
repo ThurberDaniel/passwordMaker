c689bf72c56663c4a7e121f5b44c91a27ad4ad73
ThurberDaniel <phxburg@gmail.com>
2021-10-01 11:11:30 -0600

    fri

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'generator/index.html', {'password': "98uhbhjnkjoi8hg7uybu"})
+
+
+def cars(request):
+    return HttpResponse('<h1>cars are here</h1>')
