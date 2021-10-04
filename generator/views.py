0757415286e78beb59a3737a48ce70bd4864d2f8
ThurberDaniel <phxburg@gmail.com>
2021-10-03 20:43:34 -0600

    sun

--- generator/views.py
+++ generator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'generator/index.html', {'passwords': "98uhbhjnkjoi8hg7uybu"})
+    return render(request, 'generator/index.html', {'password': "98uhbhjnkjoi8hg7uybu"})
 
 
 def cars(request):
