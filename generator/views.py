1ce0cfa87397798735fb0cc16ddb674cad04641f
ThurberDaniel <phxburg@gmail.com>
2021-10-20 09:43:58 -0600

    wed

--- generator/views.py
+++ generator/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return render(request, 'generator/index.html', {'passwords': "98u88888888kjoi8hg7uybu"})
-
-
 def cars(request):
     return HttpResponse('<h1>Cars here</h1>')
+
+
+def home(request):
+    return render(request, 'generator/index.html', {'passwords': "98u88888888kjoi8hg7uybu"})
