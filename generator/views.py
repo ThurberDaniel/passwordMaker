d6980703f8168df442bbc57549e5f4b3b4909c59
ThurberDaniel <phxburg@gmail.com>
2021-10-29 15:26:56 -0600

    friday

--- generator/views.py
+++ generator/views.py
@@ -6,5 +6,5 @@ def home(request):
     return render(request, 'generator/index.html', {'passwords': "98u88888888kjoi8hg7uybu"})
 
 
-# def cars(request):
-#     return HttpResponse('<h1>Cars here</h1>')
+def cars(request):
+    return HttpResponse('<h1>Cars here</h1>')
